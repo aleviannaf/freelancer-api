b16570a7f2dc8c0e446a3b5e16abb6c8f1eded97
aleviannaf <alexandrevianna511@gmail.com>
2023-08-23 16:21:54 -0400

    feat (middleware): created middleware to validate body of the requests

---
+++ src/middlewares/ensureDatalsValid.middleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express"
+import { ZodTypeAny } from "zod"
+
+const ensureDatalsValidMiddleware =
+    (schema: ZodTypeAny) =>
+        (req: Request, res: Response, next: NextFunction): Response | void => {
+            const validatedData: any = schema.parse(req.body)
+
+            req.body = validatedData
+
+            return next()
+        }
+
+export default ensureDatalsValidMiddleware
